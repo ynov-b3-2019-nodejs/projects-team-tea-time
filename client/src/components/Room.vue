d9f9baf3cb6c67d690bb2006dce65320b254659d
FabienTreve <trevefabien@gmail.com>
2019-05-28 17:53:48 +0200

    Working on views

---
+++ client/src/components/Room.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Room</h1>
+  </div>
+</template>
+
+<script>
+  export default { }
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import App from './App'
 
-Vue.config.productionTip = false
+/*Vue.config.productionTip = false*/
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [{
+    /*path: '/',
+    component: require('./components/HelloWorld.vue')*/
+    path: '/',
+    component: require('./components/Home.vue')
+  }, {
+    path: '/room/:id(\\d+)',
+    component: require('./components/Room.vue')
+  }, {
+    path: '*',
+    redirect: '/'
+  }]
+})
 
 new Vue({
+  el: '#app',
+  router,
   render: h => h(App),
 }).$mount('#app')

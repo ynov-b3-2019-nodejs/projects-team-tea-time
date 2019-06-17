1626f54abe2b50598097830f35735b8cfd91765c
FabienTreve <trevefabien@gmail.com>
2019-06-17 11:56:20 +0200

    mysql with express

--- start.js
+++ start.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const ejs = require('ejs');
+var mysql = require('mysql');
 
 const app = express();
 
-app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send('Vous êtes à l\'accueil, que puis-je pour vous ?');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "teatime",
+  password: "",
+  database: "teatime_db"
+});
+
+con.connect(function(error) {
+  //if (err) throw err;
+  if(!!error) {
+    console.log('Error');
+  }else{
+    console.log("Connecté!");
+  }
+  });
+
+app.get('/', function(req, resp) {
+    //res.setHeader('Content-Type', 'text/plain');
+    //res.send('Vous êtes à l\'accueil, que puis-je pour vous ?');
+    con.query("SELECT * FROM room", function (error, rows, fields){
+        if(!!error) {
+          console.log('Error');
+        }else {
+          console.log('Succes de la requete !\n');
+          console.log(rows);
+        }
+    });
 });
 
 app.use(function(req, res, next){
